6bc0c7b7c8919447d84588d35d40435d6f058af1
Gregory Vinogradov <scamanderlive@gmail.com>
2021-03-18 13:59:59 +0300

    feat: add scripts for popup and like buttons

---
+++ pages/index.js
@@ -0,0 +1,65 @@
+let content = document.querySelector('.content');
+let editButton = content.querySelector('.profile__edit-button');
+let name = content.querySelector('.profile__name');
+let description = content.querySelector('.profile__description');
+
+let popup = document.querySelector('.popup');
+let closePopupButton = popup.querySelector('.popup__close');
+let popupInputName = popup.querySelector('.popup__input_type_name');
+let popupInputDescription = popup.querySelector('.popup__input_type_description');
+let savePopupButton = popup.querySelector('.popup__button');
+
+let imageCard = document.querySelectorAll('.element');
+let likeButton = [];
+
+for (let i = 0; i < imageCard.length; i++) {
+    likeButton[i] = imageCard[i].querySelector('.element__button');
+}
+
+for (let i = 0; i < likeButton.length; i++) {
+    likeButton[i].addEventListener('click', function () {
+        if (likeButton[i].classList.contains('element__button_active')) {
+            likeButton[i].classList.remove('element__button_active');
+        } else {
+            likeButton[i].classList.add('element__button_active');
+        }
+    })
+}
+
+function setInputValue() {
+    popupInputName.value = name.textContent;
+    popupInputDescription.value = description.textContent;
+}
+
+function setProfileInfo() {
+    name.textContent = popupInputName.value;
+    description.textContent = popupInputDescription.value;
+}
+
+function openPopup() {
+    popup.classList.add('popup_active');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_active');
+}
+
+function setLike() {
+    likeButton.classList.add('element__button_active')
+}
+
+editButton.addEventListener('click', function() {
+    setInputValue();
+    openPopup();
+})
+
+closePopupButton.addEventListener('click', function() {
+    closePopup();
+})
+
+savePopupButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    setProfileInfo();
+    closePopup();
+});
