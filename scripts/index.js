42cc5cec6577d67c8b669e9fcdc46e58976fbb12
Gregory <scamanderlive@gmail.com>
2021-03-25 23:42:03 +0300

    fix: script fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,22 @@
 let content = document.querySelector('.content');
 let editButton = content.querySelector('.profile__edit-button');
-let name = content.querySelector('.profile__name');
-let description = content.querySelector('.profile__description');
+let userName = content.querySelector('.profile__name');
+let userDescription = content.querySelector('.profile__description');
 
 let popup = document.querySelector('.popup');
 let closePopupButton = popup.querySelector('.popup__close');
 let popupInputName = popup.querySelector('.popup__input_type_name');
 let popupInputDescription = popup.querySelector('.popup__input_type_description');
-let savePopupButton = popup.querySelector('.popup__save');
-
-let imageCard = document.querySelectorAll('.card');
-let likeButton = [];
-
-
-for (let i = 0; i < imageCard.length; i++) {
-    likeButton[i] = imageCard[i].querySelector('.card__button');
-}
-
-for (let i = 0; i < likeButton.length; i++) {
-    likeButton[i].addEventListener('click', function () {
-        if (likeButton[i].classList.contains('card__button_active')) {
-            likeButton[i].classList.remove('card__button_active');
-        } else {
-            likeButton[i].classList.add('card__button_active');
-        }
-    })
-}
-
-function setInputValue() {
-    popupInputName.value = name.textContent;
-    popupInputDescription.value = description.textContent;
-}
+let popupForm = popup.querySelector('.popup__form');
 
 function setProfileInfo() {
-    name.textContent = popupInputName.value;
-    description.textContent = popupInputDescription.value;
+    userName.textContent = popupInputName.value;
+    userDescription.textContent = popupInputDescription.value;
 }
 
 function openPopup() {
+    popupInputName.value = userName.textContent;
+    popupInputDescription.value = userDescription.textContent;
     popup.classList.add('popup_opened');
 }
 
@@ -45,16 +24,11 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', function() {
-    setInputValue();
-    openPopup();
-})
+editButton.addEventListener('click', openPopup)
 
-closePopupButton.addEventListener('click', function() {
-    closePopup();
-})
+closePopupButton.addEventListener('click', closePopup)
 
-savePopupButton.addEventListener('click', function(event) {
+popupForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
     setProfileInfo();
